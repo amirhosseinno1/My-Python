caa56f4ab8b50ff756800251578e80518d91551e
amirhosseinno1 <ahsolgi21@gmail.com>
2023-07-17 00:18:40 -0700

    zip

--- 0023_zip.py
+++ 0023_zip.py
@@ -2,9 +2,5 @@ Name = ['Ali' , 'Hossein' , 'Mohammad' , 'Morad']
 Age = [23, 24, 25, 26 , 27]
 Fieldes = ['math' , 'chemistry' , 'mechanic' , 'meta']
 for n , a , f in zip (Name , Age , Fieldes):
-<<<<<<< HEAD
-    print(n , a , f)
-=======
     print(n , a , f)
 #The length of the smallest number is printed.
->>>>>>> for
